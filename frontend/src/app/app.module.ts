9d9b669ba7bb4c112365ad471502a26c9be2f91c
levc4 <levc4kvlad@gmail.com>
2023-07-27 14:30:33 +0300

    added some components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { NgImageSliderModule } from 'ng-image-slider'; // Add this line
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import {RouterModule, Routes} from "@angular/router";
+import { PhotoCarouselComponent } from './photo-carousel/photo-carousel.component';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  // Add more routes here for other pages
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    HeaderComponent,
+    PhotoCarouselComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    NgImageSliderModule, // Add this line
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('slideAnimation', [
+      transition(':increment', [
+        style({ transform: 'translateX(0%)' }),
+        animate('2.7s', style({ transform: 'translateX(-100%)' })),
+      ]),
+      transition(':decrement', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('2.7s', style({ transform: 'translateX(0%)' })),
+      ]),
+    ]),
+  ],
 })
-export class HomeComponent {
+export class HomeComponent{
+  images: any[] = [
+    { url: 'assets/image1.jpg' },
+    { url: 'assets/image2.jpg' },
+    { url: 'assets/image3.jpg' },
+    {url:  'assets/image4.jpg'}
+    // Add more image URLs as needed
+  ];
 
+  currentSlideIndex = 0;
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    setInterval(() => {
+      this.slideToNext();
+    }, 2700);
+  }
+
+  slideToNext() {
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.images.length;
+  }
+
+  slideToPrevious() {
+    this.currentSlideIndex = (this.currentSlideIndex - 1 + this.images.length) % this.images.length;
+  }
 }
